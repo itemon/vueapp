4b59e4a405bed4893d301754d9a9ab6e9680eb6e
huangwei <itemon@gmail.com>
2019-06-29 13:14:15 +0800

    fix bug

--- src/components/pages/list/index.vue
+++ src/components/pages/list/index.vue
@@ -1,16 +1,33 @@
 <template>
   <div>
     <h2>hello {{ greeting }}</h2>
+    <div><Button @click="handleClick">Element Button</Button></div>
+    <div>
+      <el-input v-model="text" placeholder="这里输入文字"></el-input>
+    </div>
   </div>
 </template>
 
 <script>
+import { Button, Input } from 'element-ui'
+
 export default {
   name: 'list',
   data: () => {
     return {
-      greeting: 'list page'
+      greeting: 'list page',
+      text: 'hello'
     }
+  },
+  methods: {
+    handleClick () {
+      // eslint-disable-next-line
+      console.log(`click el button`)
+    }
+  },
+  components: {
+    Button,
+    'el-input': Input
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './route/'
 import store from './store/'
 
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.config.productionTip = false
 
 const app = new Vue({
